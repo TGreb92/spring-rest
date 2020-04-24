ebd5062635c4997340300f625abf99443d7e82b3
Greb <tgreb.92@gmail.com>
2020-04-24 23:17:47 +0200

    init

---
+++ src/test/java/com/tgreb/skeleton/springrestmongo/SpringRestMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgreb.skeleton.springrestmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
